32e4d1cf71aebd59da4975a985cff47b82571976
1pancho <medovmail@gmail.com>
2024-02-15 19:53:18 +0300

    Refactor: Profile page

--- src/Entities/ui/ModalSettings.tsx
+++ src/Entities/ui/ModalSettings.tsx
@@ -1,43 +1,27 @@
 import { Button, Input, Modal, Space, Text } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
+import { IconSettings } from '@tabler/icons-react';
 import { FC } from 'react';
-
-
+import { useTranslation } from 'react-i18next';
 
 export const ModalSettings: FC = () => {
     const [opened, { open, close }] = useDisclosure(false);
+    const { t } = useTranslation();
 
     return (
         <>
-            <Modal opened={opened} onClose={close} title="Обновить" centered>
+            <Modal opened={opened} onClose={close} title={t('profile.update')} centered>
                 <Text size="xxl">
-                    Имя
+                    {t('profile.name')}
                 </Text>
                 <Space h="xs" />
-                <Input placeholder="Введите имя" />
+                <Input placeholder={t('profile.enterYourName')} />
                 <Space h="md" />
-                <Button variant="filled">Обновить</Button>
+                <Button variant="filled">{t('profile.update')}</Button>
             </Modal>
             <Button onClick={open} color='#192E3E'>
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="icon icon-tabler icon-tabler-settings"
-                    color='#A0D4F9'
-                    width="22"
-                    height="22"
-                    viewBox="0 0 24 24"
-                    stroke-width="2"
-                    stroke="currentColor"
-                    fill="none"
-                    stroke-linecap="round"
-                    stroke-linejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                    <path d="M10.325 4.317c.426 -1.756 2.924 -1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543 -.94 3.31 .826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756 .426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543 -.826 3.31 -2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756 -2.924 1.756 -3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543 .94 -3.31 -.826 -2.37 -2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756 -.426 -1.756 -2.924 0 -3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94 -1.543 .826 -3.31 2.37 -2.37c1 .608 2.296 .07 2.572 -1.065z" />
-                    <path d="M9 12a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" />
-                </svg>
+                <IconSettings width="22" height="22" color='#A0D4F9' />
             </Button>
         </>
-
-
     );
 };
