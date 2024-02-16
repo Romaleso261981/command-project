a3269a210c98e935b4bc76846c66ee6dde107337
Roman lesyo <ladiginscormag@gmail.com>
2024-02-16 11:43:02 +0200

    refactor: refactor dev

---
+++ src/pages/AdminPage/ui/FormAddClient/FormAddClient.tsx
@@ -0,0 +1,37 @@
+import { Button, Flex, Group, Input, Text, Title } from '@mantine/core';
+
+import type { Users } from '@/shared/types/Types';
+
+import s from './FormAddClient.module.css';
+
+type Props = {
+  userData: Users[];
+};
+
+export const FormAddClient = (props: Props) => {
+  const { userData } = props;
+
+  return (
+    <Flex mih={50} gap="sm" justify="flex-start" align="flex-start" direction="column" wrap="wrap">
+      <Title order={1}>Клиенты</Title>
+      <Flex mih={50} gap="sm" justify="flex-start" align="flex-start" direction="row" wrap="wrap">
+        <Text size="xl">{`Всего: ${userData.length}`}</Text>
+      </Flex>
+      <Flex mih={50} gap="sm" justify="flex-start" align="flex-start" direction="row" wrap="wrap">
+        <Input.Wrapper label="Телефон">
+          <Input placeholder="+380689478723" className={s.input} />
+        </Input.Wrapper>
+        <Input.Wrapper label="Никнейм">
+          <Input placeholder="example" className={s.input} />
+        </Input.Wrapper>
+        <Input.Wrapper label="ФИО">
+          <Input placeholder="Petro Petrov" className={s.input} />
+        </Input.Wrapper>
+      </Flex>
+      <Group className={s.searchWrapper}>
+        <Input placeholder="search" className={s.input} />
+        <Button variant="filled">Збросить</Button>
+      </Group>
+    </Flex>
+  );
+};
--- src/shared/ui/Header/ui/Navbar/Navbar.tsx
+++ src/shared/ui/Header/ui/Navbar/Navbar.tsx
@@ -1,18 +1,40 @@
-import { Group, Stack } from '@mantine/core';
+import { Group, rem, Stack, Tooltip, UnstyledButton } from '@mantine/core';
 import {
-  IconDeviceDesktopAnalytics,
-  IconFingerprint,
+  IconBuildingStore,
+  IconCalendarClock,
+  IconDeviceGamepad2,
   IconHome2,
-  IconLogin,
   IconLogout,
-  IconSettings
+  IconSettings,
+  IconUser,
+  IconUsers
 } from '@tabler/icons-react';
 import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
 
 import { RoutersPaths } from '@/shared/types/enums';
 
-import { NavbarLink } from '../NavbarLink/NavbarLink';
 import classes from './Navbar.module.css';
+
+interface NavbarLinkProps {
+  icon: typeof IconHome2;
+  label: string;
+  path: string;
+  active?: boolean;
+  onClick?(): void;
+}
+
+function NavbarLink({ icon: Icon, label, active, onClick, path }: NavbarLinkProps) {
+  return (
+    <Tooltip label={label} position="right" transitionProps={{ duration: 0 }}>
+      <UnstyledButton onClick={onClick} className={classes.link} data-active={active || undefined}>
+        <Link to={path}>
+          <Icon style={{ width: rem(20), height: rem(20) }} stroke={1.5} />
+        </Link>
+      </UnstyledButton>
+    </Tooltip>
+  );
+}
 
 export const Navbar = () => {
   const { t } = useTranslation();
@@ -21,18 +43,28 @@ export const Navbar = () => {
     <nav className={classes.navbar}>
       <Stack justify="space-between" h={550} gap={20}>
         <Group>
-          <NavbarLink path={RoutersPaths.LOGIN} icon={IconLogin} label={t('auth.login')} />
-          <NavbarLink path={RoutersPaths.MAIN} icon={IconHome2} label={t('navBar.home')} />
+          <NavbarLink path={RoutersPaths.ADMIN} icon={IconHome2} label={t('navBar.home')} />
           <NavbarLink
             path={RoutersPaths.ADMIN}
-            icon={IconDeviceDesktopAnalytics}
-            label={t('navBar.admin')}
+            icon={IconDeviceGamepad2}
+            label={t('navBar.game')}
           />
-          <NavbarLink path={RoutersPaths.MAIN} icon={IconFingerprint} label={t('navBar.profile')} />
+          <NavbarLink
+            path={RoutersPaths.LOGIN}
+            icon={IconCalendarClock}
+            label={t('navBar.schedule')}
+          />
+          <NavbarLink
+            path={RoutersPaths.ADMIN}
+            icon={IconBuildingStore}
+            label={t('navBar.store')}
+          />
+          <NavbarLink path={RoutersPaths.MAIN} icon={IconUser} label={t('navBar.user')} />
+          <NavbarLink path={RoutersPaths.ADMIN} icon={IconUsers} label={t('navBar.users')} />
         </Group>
         <Group>
-          <NavbarLink path={RoutersPaths.MAIN} icon={IconSettings} label="Settings" />
-          <NavbarLink path={RoutersPaths.MAIN} icon={IconLogout} label="Logout" />
+          <NavbarLink path={RoutersPaths.ADMIN} icon={IconSettings} label={t('navBar.settings')} />
+          <NavbarLink path={RoutersPaths.ADMIN} icon={IconLogout} label={t('navBar.logout')} />
         </Group>
       </Stack>
     </nav>
