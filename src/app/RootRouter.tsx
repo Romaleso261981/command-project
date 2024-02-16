d0a72e42856188d26a92fec6613327b66ce3b18c
Roman lesyo <ladiginscormag@gmail.com>
2024-02-16 12:40:10 +0200

    refactor: refactor dev

--- src/app/RootRouter.tsx
+++ src/app/RootRouter.tsx
@@ -4,12 +4,12 @@ import { Route, Routes } from 'react-router-dom';
 import { Layout } from '@/app/layout';
 import { RoutersPaths } from '@/shared/types/enums';
 import { Spiner } from '@/shared/ui/Loader/Loader';
-const MainPage = lazy(() => import('../pages/MainPage/MainPage'));
-const UserPage = lazy(() => import('../pages/UserPage/UserPage'));
-const AuthPage = lazy(() => import('@/pages/AuthPage/index'));
-const AdminPage = lazy(() => import('../pages/AdminPage/AdminPage'));
-const NotFound = lazy(() => import('../pages/Error-404/Error-404'));
-const ProfilePage = lazy(() => import('../pages/ProfilePage/ProfilePage'));
+const MainPage = lazy(() => import('@/pages/MainPage/MainPage'));
+const UserPage = lazy(() => import('@/pages/UserPage/UserPage'));
+const AuthPage = lazy(() => import('@/pages/AuthPage'));
+const AdminPage = lazy(() => import('@/pages/AdminPage/AdminPage'));
+const NotFound = lazy(() => import('@/pages/Error-404/Error-404'));
+const ProfilePage = lazy(() => import('@/pages/ProfilePage/ProfilePage'));
 
 export default function RootRouter() {
   return (
